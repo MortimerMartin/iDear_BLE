1d3754ecc72e6bd08107cc8cbdd34a0646fac8c9
dorayo <yomingyo@gmail.com>
2017-02-10 10:35:25 +0800

    Initial Commit

---
+++ SuperWatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperWatch
+//
+//  Created by pro on 17/2/10.
+//  Copyright © 2017年 pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SuperWatch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SuperWatch
+//
+//  Created by pro on 17/2/10.
+//  Copyright © 2017年 pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
